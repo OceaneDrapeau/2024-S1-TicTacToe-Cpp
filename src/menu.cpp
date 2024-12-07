89c01070c6faa730454235982390495a0cb8a59b
ocedr <ocedrapeau@gmail.com>
2024-12-07 16:39:57 +0100

    update verification start_menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -8,7 +8,21 @@ int start_menu()
     std::cout << "Bienvenue dans le jeu du TicTacToe \nVeuillez choisir un mode de jeu :" << std::endl;
 
     std::cout << "1. Deux joueurs \n2. Un joueur contre l'IA" << std::endl;
-    std::cin >> choice;
+
+    do
+    {
+        std::cout << "Mode : ";
+        std::cin >> choice;
+
+        if (std::cin.fail() || (choice != 1 && choice != 2))
+        {
+            std::cin.clear();
+            std::cin.ignore(255, '\n');
+            std::cout << "Choisir un mode valide. Réessayez." << std::endl;
+        }
+    } while (choice != 1 && choice != 2);
+
+    std::cin.ignore(255, '\n');
 
     return choice;
 }
