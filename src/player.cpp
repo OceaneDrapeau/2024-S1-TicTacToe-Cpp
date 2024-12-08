f7178c56c033ab9a5a1032e1204592c66b9f5a82
ocedr <ocedrapeau@gmail.com>
2024-12-08 21:42:27 +0100

    fix create_player

--- src/player.cpp
+++ src/player.cpp
@@ -1,56 +1,54 @@
 #include "player.hpp"
 
-Player create_player()
+// Name of the player
+std::string playerName()
+{
+    std::string nameField{"Nom : "};
+    std::string errorMessage{"Le nom ne peut pas être vide. Réessayez."};
+    std::string name{getValidInputStr(nameField, errorMessage)};
+
+    return name;
+}
+
+// Symbol of the player
+char playerSymbol(char symbol1, char symbol2)
+{
+    char symbol{};
+    std::string firstSymbol{};
+    std::string secondSymbol{};
+    firstSymbol.push_back(symbol1);
+    secondSymbol.push_back(symbol2);
+
+    std::string nameField{"Symbole (" + firstSymbol + " ou " + secondSymbol + ") : "};
+    std::string errorMessage{"Choisir un symbole valide. Réessayez."};
+    std::vector<char> validInputs{'X', 'O'};
+    char symbol{getValidInputChar(nameField, errorMessage, validInputs)};
+
+    symbol = (std::toupper(symbol) == std::toupper(symbol1)) ? symbol1 : symbol2;
+
+    return symbol;
+}
+
+Player create_player(char symbol1, char symbol2)
 {
     // Initializations
     Player player{};
-    std::string tempSymbol{};
-    std::string const symbol1{"X"};
-    std::string const symbol2{"O"};
+    char tempSymbol{};
 
-    // Name of the player
-    do
-    {
-        std::cout << "Nom : ";
-        std::getline(std::cin, player.name);
-
-        if (player.name.empty())
-        {
-            std::cout << "Le nom ne peut pas être vide. Réessayez." << std::endl;
-        }
-    } while (player.name.empty());
-
-    // Symbol of the player
-    do
-    {
-        std::cout
-            << "Symbole ('" << symbol1 << "' ou '" << symbol2 << "') : ";
-
-        std::getline(std::cin, tempSymbol);
-
-        if (tempSymbol.size() == 1)
-        {
-            tempSymbol[0] = std::toupper(tempSymbol[0]);
-        }
-
-        if (tempSymbol != symbol1 && tempSymbol != symbol2)
-        {
-            std::cout << "Saisir un symbole valide." << std::endl;
-        }
-        else
-        {
-            player.symbol = tempSymbol[0];
-        };
-
-    } while (tempSymbol != symbol1 && tempSymbol != symbol2);
+    player.name = playerName();
+    player.symbol = playerSymbol(symbol1, symbol2);
 
     return player;
 };
 
-Player create_AI(char symbolPlayer, char symbol1, char symbol2)
+Player create_player(Player player1, char symbol1, char symbol2, bool AI)
 {
-    Player AI{};
-    AI.name = "AI";
-    AI.symbol = (symbolPlayer == symbol1) ? symbol2 : symbol1;
-    return AI;
+    Player player{};
+
+    player.name = (AI) ? "AI" : playerName();
+
+    char symbolPlayer1{player1.symbol};
+    player.symbol = (symbolPlayer1 == symbol1) ? symbol2 : symbol1;
+
+    return player;
 }
--- src/player.hpp
+++ src/player.hpp
@@ -1,6 +1,7 @@
+#pragma once
 #include <iostream>
 #include <string>
-#pragma once
+#include "verification.hpp"
 
 struct Player
 {
@@ -8,5 +9,5 @@ struct Player
     char symbol;
 };
 
-Player create_player();
-Player create_AI(char symbolPlayer, char symbol1 = 'X', char symbol2 = 'O');
+Player create_player(char symbol1 = 'X', char symbol2 = 'O');
+Player create_player(char symbolPlayer, char symbol1 = 'X', char symbol2 = 'O', bool AI = true);
